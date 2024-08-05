aea6cd4417e129f2e15f9a8be899f0af7b0f5110
kyuuinochi <kyuuinochi@gmail.com>
2024-08-05 10:45:39 +0800

    add copybutton

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,9 @@ author = 'www.sunfounder.com'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autosectionlabel'
+extensions = [
+    'sphinx.ext.autosectionlabel',
+    'sphinx_copybutton'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
