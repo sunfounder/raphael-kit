dfb3da7781146418382096bfad4bc27220a5d871
kyuuinochi <kyuuinochi@gmail.com>
2024-08-09 15:30:46 +0800

    update dht11 python

--- python-pi5/2.2.3_DHT.py
+++ python-pi5/2.2.3_DHT.py
@@ -1,29 +1,81 @@
+from gpiozero import OutputDevice, InputDevice
 import time
-import board
-import adafruit_dht
 
-# Initial the dht device, with data pin connected to:
-dhtDevice = adafruit_dht.DHT11(board.D17)
 
-while True:
-    try:
-        # Print the values to the serial port
-        temperature_c = dhtDevice.temperature
-        temperature_f = temperature_c * (9 / 5) + 32
-        humidity = dhtDevice.humidity
-        print(
-            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
-                temperature_f, temperature_c, humidity
-            )
-        )
+class DHT11():
+    MAX_DELAY_COUINT = 100
+    BIT_1_DELAY_COUNT = 10
+    BITS_LEN = 40
 
-    except RuntimeError as error:
-        # Errors happen fairly often, DHT's are hard to read, just keep going
-        print(error.args[0])
-        time.sleep(2.0)
-        continue
-    except Exception as error:
-        dhtDevice.exit()
-        raise error
+    def __init__(self, pin, pull_up=False):
+        self._pin = pin
+        self._pull_up = pull_up
 
-    time.sleep(2.0)
+
+    def read_data(self):
+        bit_count = 0
+        delay_count = 0
+        bits = ""
+
+        # -------------- send start --------------
+        gpio = OutputDevice(self._pin)
+        gpio.off()
+        time.sleep(0.02)
+
+        gpio.close()
+        gpio = InputDevice(self._pin, pull_up=self._pull_up)
+
+        # -------------- wait response --------------
+        while gpio.value == 1:
+            pass
+        
+        # -------------- read data --------------
+        while bit_count < self.BITS_LEN:
+            while gpio.value == 0:
+                pass
+
+            # st = time.time()
+            while gpio.value == 1:
+                delay_count += 1
+                # break
+                if delay_count > self.MAX_DELAY_COUINT:
+                    break
+            if delay_count > self.BIT_1_DELAY_COUNT:
+                bits += "1"
+            else:
+                bits += "0"
+
+            delay_count = 0
+            bit_count += 1
+
+        # -------------- verify --------------
+        humidity_integer = int(bits[0:8], 2)
+        humidity_decimal = int(bits[8:16], 2)
+        temperature_integer = int(bits[16:24], 2)
+        temperature_decimal = int(bits[24:32], 2)
+        check_sum = int(bits[32:40], 2)
+
+        _sum = humidity_integer + humidity_decimal + temperature_integer + temperature_decimal
+
+        # print(bits)
+        # print(humidity_integer, humidity_decimal, temperature_integer, temperature_decimal)
+        # print(f'sum:{_sum}, check_sum:{check_sum}')
+        # print()
+
+        if check_sum != _sum:
+            humidity = 0.0
+            temperature = 0.0
+        else:
+            humidity = float(f'{humidity_integer}.{humidity_decimal}')
+            temperature = float(f'{temperature_integer}.{temperature_decimal}')
+
+        # -------------- return --------------
+        return humidity, temperature
+
+
+if __name__ == '__main__':
+    dht11 = DHT11(17)
+    while True:
+        humidity, temperature = dht11.read_data()
+        print(f"{time.time():.3f}  temperature:{temperature}°C  humidity: {humidity}%")
+        time.sleep(2)
